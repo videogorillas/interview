378476339f4750281781e6658953653079c290b2
Alex Zhukov <zhukov.alex@gmail.com>
2017-07-26 12:04:54 +0300

    100 and 1000

--- src/test/java/com/vg/InterviewTest.java
+++ src/test/java/com/vg/InterviewTest.java
@@ -7,12 +7,20 @@ import org.junit.Test;
 public class InterviewTest {
     @Test
     public void testName() throws Exception {
-        Integer a = 1000;
-        Integer b = 1000;
+        Integer a = 100;
+        Integer b = 100;
          
         System.out.println(a.equals(b));
 
         System.out.println(a == b);
+        
+        Integer a2 = 1000;
+        Integer b2 = 1000;
+         
+        System.out.println(a2.equals(b2));
+
+        System.out.println(a2 == b2);
+
     }
 
 }
