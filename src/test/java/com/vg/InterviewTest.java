1310c670049f42bdff48b63755f0bdbaa09a7bc3
Alex Zhukov <zhukov.alex@gmail.com>
2017-07-26 12:04:17 +0300

    first one

---
+++ src/test/java/com/vg/InterviewTest.java
@@ -0,0 +1,18 @@
+package com.vg;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InterviewTest {
+    @Test
+    public void testName() throws Exception {
+        Integer a = 1000;
+        Integer b = 1000;
+         
+        System.out.println(a.equals(b));
+
+        System.out.println(a == b);
+    }
+
+}
